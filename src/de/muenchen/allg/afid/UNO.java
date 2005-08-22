4e54a8b21c348e741c86863faf2193ba532f18c1
bnk <devnull@localhost>
2005-08-22 12:49:22 +0200

    [svn r203] (BNK) Nr. entfernt wg. dauernder Synchronisationsprobleme

--- src/de/muenchen/allg/afid/UNO.java
+++ src/de/muenchen/allg/afid/UNO.java
@@ -7,22 +7,22 @@
 * Copyright: Landeshauptstadt München
 *
 * Änderungshistorie:
-* Nr. |  Datum     | Wer | Änderungsgrund
+* Datum     | Wer | Änderungsgrund
 * -------------------------------------------------------------------
-* 001 | 26.04.2005 | BNK | Erstellung
-* 002 | 07.07.2005 | BNK | Viele Verbesserungen
-* 003 | 16.08.2005 | BNK | korrekte Dienststellenbezeichnung
-* 004 | 17.08.2005 | BNK | +executeMacro()
-* 005 | 17.08.2005 | BNK | +Internal-Klasse für interne Methoden
-* 006 | 17.08.2005 | BNK | +findBrowseNodeTreeLeaf()
-* 007 | 19.08.2005 | BNK | init(Object) => public, weil nützlich
-* 008 | 19.08.2005 | BNK | +XPrintable()
-* 009 | 22.08.2005 | PIT | +XNamed() 
-* 010 | 22.08.2005 | PIT | +XTextContent()  
-* 011 | 19.08.2005 | BNK | +XSpreadsheetDocument()
-* 012 | 19.08.2005 | BNK | +XIndexAccess()
-* 013 | 19.08.2005 | BNK | +XCellRange()
-* 014 | 22.08.2005 | BNK | +XText()
+* 26.04.2005 | BNK | Erstellung
+* 07.07.2005 | BNK | Viele Verbesserungen
+* 16.08.2005 | BNK | korrekte Dienststellenbezeichnung
+* 17.08.2005 | BNK | +executeMacro()
+* 17.08.2005 | BNK | +Internal-Klasse für interne Methoden
+* 17.08.2005 | BNK | +findBrowseNodeTreeLeaf()
+* 19.08.2005 | BNK | init(Object) => public, weil nützlich
+* 19.08.2005 | BNK | +XPrintable()
+* 22.08.2005 | PIT | +XNamed() 
+* 22.08.2005 | PIT | +XTextContent()  
+* 19.08.2005 | BNK | +XSpreadsheetDocument()
+* 19.08.2005 | BNK | +XIndexAccess()
+* 19.08.2005 | BNK | +XCellRange()
+* 22.08.2005 | BNK | +XText()
 * ------------------------------------------------------------------- 
 *
 * @author D-III-ITD 5.1 Matthias S. Benkmann
