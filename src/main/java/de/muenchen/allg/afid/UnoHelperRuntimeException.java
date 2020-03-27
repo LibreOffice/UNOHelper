6a80b83eec14eaf1d61f1e7410d350f6a1a5022c
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-03-27 14:45:58 +0100

    sonar fixes for exceptions

--- src/main/java/de/muenchen/allg/afid/UnoHelperRuntimeException.java
+++ src/main/java/de/muenchen/allg/afid/UnoHelperRuntimeException.java
@@ -1,19 +1,42 @@
 package de.muenchen.allg.afid;
 
+/**
+ * Runtime exception in UnoHelper.
+ */
 public class UnoHelperRuntimeException extends RuntimeException
 {
   private static final long serialVersionUID = 1L;
 
+  /**
+   * Create new exception.
+   *
+   * @param msg
+   *          The message of the exception.
+   * @param e
+   *          The cause.
+   */
   public UnoHelperRuntimeException(String msg, Throwable e)
   {
     super(msg, e);
   }
 
+  /**
+   * Create new exception.
+   *
+   * @param msg
+   *          The message of the exception.
+   */
   public UnoHelperRuntimeException(String msg)
   {
     super(msg);
   }
 
+  /**
+   * Create new exception.
+   *
+   * @param e
+   *          The cause of the exception.
+   */
   public UnoHelperRuntimeException(Throwable e)
   {
     super(e);
