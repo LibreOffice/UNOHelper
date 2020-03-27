080b413338845b57a9cf6a086559dc1a54ba99d0
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-03-27 14:45:58 +0100

    sonar fixes for exceptions

--- src/main/java/de/muenchen/allg/afid/UnoHelperException.java
+++ src/main/java/de/muenchen/allg/afid/UnoHelperException.java
@@ -1,19 +1,42 @@
 package de.muenchen.allg.afid;
 
+/**
+ * Exception occurred in UnoHelper.
+ */
 public class UnoHelperException extends Exception
 {
   private static final long serialVersionUID = 1L;
 
+  /**
+   * Create new exception.
+   *
+   * @param msg
+   *          The message of the exception.
+   * @param e
+   *          The cause.
+   */
   public UnoHelperException(String msg, Throwable e)
   {
     super(msg, e);
   }
 
+  /**
+   * Create new exception.
+   *
+   * @param msg
+   *          The message of the exception.
+   */
   public UnoHelperException(String msg)
   {
     super(msg);
   }
 
+  /**
+   * Create new exception.
+   *
+   * @param e
+   *          The cause of the exception.
+   */
   public UnoHelperException(Throwable e)
   {
     super(e);
