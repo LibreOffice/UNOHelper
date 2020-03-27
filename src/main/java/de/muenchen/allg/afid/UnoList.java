5162c5d5e159468dad52ba6e6d761219feb7b1cc
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-03-27 14:47:32 +0100

    sonar fixes for UnoList

--- src/main/java/de/muenchen/allg/afid/UnoList.java
+++ src/main/java/de/muenchen/allg/afid/UnoList.java
@@ -5,36 +5,67 @@ import java.util.AbstractList;
 import com.sun.star.container.XIndexAccess;
 import com.sun.star.lang.IndexOutOfBoundsException;
 import com.sun.star.lang.WrappedTargetException;
+import com.sun.star.uno.UnoRuntime;
 
 /**
- * Wrappt XIndexAccess als JAVA-List.
+ * Wrapper for {@link XIndexAccess}.
+ *
+ * @param <T>
+ *          type of objects in the list.
  */
 public class UnoList<T> extends AbstractList<T>
 {
-	private XIndexAccess access;
+  /**
+   * The access to the objects.
+   */
+  private XIndexAccess access;
 
-	public UnoList(Object o)
-	{
-		access = UNO.XIndexAccess(o);
-	}
+  /**
+   * Create new UnoList.
+   *
+   * @param o
+   *          An object which implements {@link XIndexAccess}.
+   */
+  public UnoList(Object o)
+  {
+    access = UNO.XIndexAccess(o);
+  }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public T get(int index)
-	{
-		try
-		{
-			return (T) access.getByIndex(index);
-		} catch (IndexOutOfBoundsException | WrappedTargetException e)
-		{
-			return null;
-		}
-	}
+  @SuppressWarnings("unchecked")
+  @Override
+  public T get(int index)
+  {
+    try
+    {
+      return (T) access.getByIndex(index);
+    } catch (IndexOutOfBoundsException | WrappedTargetException e)
+    {
+      return null;
+    }
+  }
 
-	@Override
-	public int size()
-	{
-		return access.getCount();
-	}
+  @Override
+  public int size()
+  {
+    return access.getCount();
+  }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    boolean equal = super.equals(o);
+    if (equal && o instanceof UnoList)
+    {
+      UnoList<?> other = (UnoList<?>) o;
+      return UnoRuntime.areSame(access, other.access);
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return super.hashCode() + access.hashCode();
+  }
 
 }
