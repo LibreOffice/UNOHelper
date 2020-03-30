63fb86b7ef92d281fd21dbd527757be9b4cd24b7
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-03-30 15:39:23 +0200

    fix sonar warnings in TextDocument

--- src/main/java/de/muenchen/allg/ooo/TextDocument.java
+++ src/main/java/de/muenchen/allg/ooo/TextDocument.java
@@ -1,38 +1,3 @@
-/*
-* Dateiname: TextDocument.java
-* Projekt  : UNOHelper
-* Funktion : Hilfsfunktionen für die Arbeit mit OOo TextDokumenten
-* 
-* Copyright (c) 2008 Landeshauptstadt München
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the European Union Public Licence (EUPL),
-* version 1.0 (or any later version).
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* European Union Public Licence for more details.
-*
-* You should have received a copy of the European Union Public Licence
-* along with this program. If not, see
-* http://ec.europa.eu/idabc/en/document/7330
-*
-* Änderungshistorie:
-* Datum      | Wer | Änderungsgrund
-* -------------------------------------------------------------------
-* 19.12.2007 | BNK | Erstellung
-* 15.01.2008 | BNK | +copyDirectValueCharAttributes
-* 29.01.2008 | BNK | +copySimpleProperties()
-* 29.01.2008 | BNK | +deleteParagraph
-* 30.01.2008 | BNK | +disappearParagraph
-* 03.03.2010 | ERT | +getBookmarkNamesStartingWith
-* 17.03.2011 | BED | isSimpleType um Prüfung auf Void und Type erweitert
-* -------------------------------------------------------------------
-*
-* @author Matthias Benkmann (D-III-ITD 5.1)
-* 
-*/
 package de.muenchen.allg.ooo;
 
 import java.util.ArrayList;
@@ -45,6 +10,7 @@ import com.sun.star.container.NoSuchElementException;
 import com.sun.star.container.XEnumerationAccess;
 import com.sun.star.container.XNamed;
 import com.sun.star.text.XTextContent;
+import com.sun.star.text.XTextDocument;
 import com.sun.star.text.XTextRange;
 
 import de.muenchen.allg.afid.UNO;
@@ -54,67 +20,61 @@ import de.muenchen.allg.afid.UnoIterator;
 import de.muenchen.allg.util.UnoProperty;
 
 /**
- * Hilfsfunktionen für die Arbeit mit OOo TextDokumenten
- *
- * @author Matthias Benkmann (D-III-ITD 5.1)
+ * Helper for working with {@link XTextDocument}s.
  */
 public class TextDocument
 {
+  private TextDocument()
+  {
+    // nothing to do
+  }
+
   /**
-   * Löscht den ganzen ersten Absatz an der Cursorposition textCursor.
+   * Delete the first paragraph at the cursor position.
+   *
+   * @param range
+   *          The cursor position.
+   * @throws UnoHelperRuntimeException
+   *           Paragraph can't be deleted.
    */
   public static void deleteParagraph(XTextRange range)
   {
-    // Beim Löschen des Absatzes erzeugt OOo ein ungewolltes
-    // "Zombie"-Bookmark.
-    // Issue Siehe http://qa.openoffice.org/issues/show_bug.cgi?id=65247
-
-    XTextContent paragraph = null;
-
-    // Ersten Absatz des Bookmarks holen:
     XEnumerationAccess access = UNO.XEnumerationAccess(range);
     if (access != null)
     {
-    	paragraph = new UnoIterator<XTextContent>(access, XTextContent.class).next();
-    }
+      // get first paragraph
+      XTextContent paragraph = new UnoIterator<XTextContent>(access, XTextContent.class).next();
 
-    if (paragraph != null)
-    {
-	    // Lösche den Paragraph
-	    try
-	    {
-	      // Ist der Paragraph der einzige Paragraph des Textes, dann kann er mit
-	      // removeTextContent nicht gelöscht werden. In diesme Fall wird hier
-	      // wenigstens der Inhalt entfernt:
-	      paragraph.getAnchor().setString("");
-	
-	      // Paragraph löschen
-	      range.getText().removeTextContent(paragraph);
-	    } catch (NoSuchElementException e)
-	    {
-	      // sollte eigentlich nicht passieren können
-	    }
+      if (paragraph != null)
+      {
+        try
+        {
+          // If the range has only one paragraph it can't be deleted by removeTextContent. So we
+          // only overwrite the content with an empty string.
+          paragraph.getAnchor().setString("");
+          range.getText().removeTextContent(paragraph);
+        } catch (NoSuchElementException e)
+        {
+          throw new UnoHelperRuntimeException(e);
+        }
+      }
     }
   }
 
   /**
-   * Liefert die Namen aller Bookmarks, die in im Bereich range existieren und regex matchen.
-   * Hinweis: Die Funktion liefert nur Bookmarks zurück, von denen Anfang UND Ende in dem Bereich
-   * liegen (die schließt kollabierte Bookmarks mit ein). OpenOffice.org hatte in diesem Bereich
-   * einige Probleme (oder hat sie immer noch), so dass ich nicht sicher bin, ob dies überall
-   * gewährleistet ist. Insbesondere bei TextRanges in Tabellen meine ich mich erinnern zu können,
-   * dass die Enumeration immer die ganze Zelle liefert anstatt den ausgewählten Bereich. Es ist
-   * also nicht auszuschließen, dass diese Methode unter bestimmten Umständen zu viele Bookmarks
-   * zurückliefert.
+   * Get the names of all the book marks in the range matching the regular expression. Also names of
+   * collapsed book marks are returned. Only book marks which are fully in the range are included.
+   *
+   * In some special cases like tables this method may return more names than in the text range.
    * 
-   * @throws UnoHelperRuntimeException
+   * @param regex
+   *          A regular expression for the names of the book marks.
+   * @param range
+   *          The range in which we are looking for book marks.
+   * @return All names of book marks in the range matching the regular expression.
    */
   public static Set<String> getBookmarkNamesMatching(Pattern regex, XTextRange range)
   {
-    // Hier findet eine iteration des über den XEnumerationAccess des ranges
-    // statt. Man könnte statt dessen auch über range-compare mit den bereits
-    // bestehenden Blöcken aus TextDocumentModel.get<blockname>Blocks()
-    // vergleichen...
     HashSet<String> found = new HashSet<>();
     HashSet<String> started = new HashSet<>();
 
@@ -150,9 +110,14 @@ public class TextDocument
   }
 
   /**
-   * Gibt eine Liste aller Bookmarks in einer TextRange zurück.
+   * Get a list of all book marks in a text range.
+   *
+   * @param range
+   *          The text range.
+   * @return The list of book marks.
    *
    * @throws UnoHelperRuntimeException
+   *           Can't access a book mark.
    */
   public static List<XNamed> getBookmarkByTextRange(XTextRange range)
   {
