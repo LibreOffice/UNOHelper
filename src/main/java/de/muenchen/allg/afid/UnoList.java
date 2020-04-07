76c656eb301bd81f81650805378f51b5b3f8e18c
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-04-07 13:50:17 +0200

    factory methods for UnoList

--- src/main/java/de/muenchen/allg/afid/UnoList.java
+++ src/main/java/de/muenchen/allg/afid/UnoList.java
@@ -3,6 +3,7 @@ package de.muenchen.allg.afid;
 import java.util.AbstractList;
 
 import com.sun.star.container.XIndexAccess;
+import com.sun.star.container.XNameAccess;
 import com.sun.star.lang.IndexOutOfBoundsException;
 import com.sun.star.lang.WrappedTargetException;
 import com.sun.star.uno.UnoRuntime;
@@ -21,14 +22,65 @@ public class UnoList<T> extends AbstractList<T>
   private XIndexAccess access;
 
   /**
+   * The type of objects.
+   */
+  private Class<T> c;
+
+  /**
    * Create new UnoList.
    *
-   * @param o
+   * @param access
    *          An object which implements {@link XIndexAccess}.
+   * @param c
+   *          The type of the objects.
    */
-  public UnoList(Object o)
+  private UnoList(XIndexAccess access, Class<T> c)
   {
-    access = UNO.XIndexAccess(o);
+    this.access = access;
+    this.c = c;
+  }
+
+  /**
+   * Create an UnoList for the provided mapping.
+   *
+   * @param <T>
+   *          The type of the objects in the mapping.
+   * @param access
+   *          The mapping.
+   * @param c
+   *          The type of the objects.
+   * @return An UnoList.
+   */
+  public static <T> UnoList<T> create(XIndexAccess access, Class<T> c)
+  {
+    return new UnoList<>(access, c);
+  }
+
+  /**
+   * Create an UnoList for the provided object. The object must implement {@link XNameAccess}.
+   *
+   * @param <T>
+   *          The type of the objects in the enumeration.
+   * @param o
+   *          The mapping.
+   * @param c
+   *          The type of the objects.
+   * @return An UnoList or null if object doesn't implement {@link XNameAccess}.
+   */
+  public static <T> UnoList<T> create(Object o, Class<T> c)
+  {
+    XIndexAccess access = UNO.XIndexAccess(o);
+    if (access != null)
+    {
+      return new UnoList<>(access, c);
+    }
+
+    return null;
+  }
+
+  public XIndexAccess getAccess()
+  {
+    return access;
   }
 
   @SuppressWarnings("unchecked")
@@ -37,7 +89,13 @@ public class UnoList<T> extends AbstractList<T>
   {
     try
     {
-      return (T) access.getByIndex(index);
+      if (c != Object.class)
+      {
+        return UnoRuntime.queryInterface(c, access.getByIndex(index));
+      } else
+      {
+        return (T) access.getByIndex(index);
+      }
     } catch (IndexOutOfBoundsException | WrappedTargetException e)
     {
       return null;
