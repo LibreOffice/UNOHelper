cf4e88f2a7ce47f18a4eaf37ee5fc09436336e29
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-06-24 15:35:51 +0200

    remove old license header

--- src/main/java/de/muenchen/allg/ooo/MemoryStorage.java
+++ src/main/java/de/muenchen/allg/ooo/MemoryStorage.java
@@ -1,34 +1,3 @@
-/*
- * Dateiname: MemoryStorage.java
- * Projekt  : UNOHelper
- * Funktion : Definiert einen UNO-Service Storage zum Speichern von
- *            Dokumenten in den Hauptspeicher. 
- * 
- * Copyright (c) 2008 Landeshauptstadt München
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the European Union Public Licence (EUPL),
- * version 1.0 (or any later version).
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * European Union Public Licence for more details.
- *
- * You should have received a copy of the European Union Public Licence
- * along with this program. If not, see
- * http://ec.europa.eu/idabc/en/document/7330
- *
- * Änderungshistorie:
- * Datum      | Wer | Änderungsgrund
- * -------------------------------------------------------------------
- * 04.12.2007 | LUT | Erstellung
- * -------------------------------------------------------------------
- *
- * @author Christoph Lutz (D-III-ITD-D101)
- * @version 1.0
- * 
- */
 package de.muenchen.allg.ooo;
 
 import java.io.ByteArrayInputStream;
