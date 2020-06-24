799a9bb925c48987755eb615b7e115e5f6df38bb
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-06-24 15:35:51 +0200

    remove old license header

--- src/main/java/de/muenchen/allg/afid/BrowseNode.java
+++ src/main/java/de/muenchen/allg/afid/BrowseNode.java
@@ -1,38 +1,3 @@
-/*
- * Hilfen für die Arbeit mit BrowseNodes
-* Dateiname: BrowseNode.java
-* Projekt  : n/a
-* Funktion : Hilfen für die Arbeit mit BrowseNodes
-* 
- * Copyright (c) 2008 Landeshauptstadt München
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the European Union Public Licence (EUPL),
- * version 1.0 (or any later version).
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * European Union Public Licence for more details.
- *
- * You should have received a copy of the European Union Public Licence
- * along with this program. If not, see
- * http://ec.europa.eu/idabc/en/document/7330
-*
-* Änderungshistorie:
-*  Datum     | Wer | Änderungsgrund
-* -------------------------------------------------------------------
-* 07.07.2005 | BNK | Erstellung
-* 16.08.2005 | BNK | korrekte Dienststellenbezeichnung
-* 17.08.2005 | BNK | bessere Kommentare
-* 31.08.2005 | BNK | +children(), um nur Kinder durchzuiterieren
-* 13.09.2005 | BNK | besserer Kommentar zu URL()
-* -------------------------------------------------------------------
-*
-* @author D-III-ITD 5.1 Matthias S. Benkmann
-* @version 1.0
-* 
-*/
 package de.muenchen.allg.afid;
 
 import java.util.Iterator;
