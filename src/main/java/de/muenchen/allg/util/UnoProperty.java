6605a850ac99dae502743e4fc6e554d7be8f2768
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-07-16 14:51:07 +0200

    add properties for hiding bookmarks

--- src/main/java/de/muenchen/allg/util/UnoProperty.java
+++ src/main/java/de/muenchen/allg/util/UnoProperty.java
@@ -48,6 +48,8 @@ public final class UnoProperty
   public static final String BACK_TRANSPARENT = "BackTransparent";
   public static final String BACKGROUND_COLOR = "BackgroundColor";
   public static final String BOOKMARK = "Bookmark";
+  public static final String BOOKMARK_HIDDEN = "BookmarkHidden";
+  public static final String BOOKMARK_CONDITION = "BookmarkCondition";
   public static final String BORDER = "Border";
   public static final String BORDER_COLOR = "BorderColor";
   public static final String BORDER_DISTANCE = "BorderDistance";
